2c4ebcd25500f29dfee180227c0b632fe8114de6
darealbirdie <shoubhitmukhe@gmail.com>
2025-06-18 10:28:49 -0400

    Update muse.py

--- muse.py
+++ muse.py
@@ -14,7 +14,7 @@ from typing import Dict, Optional
 from dotenv import load_dotenv
 import os
 from os import system
-from paypalrestsdk import Payment
+import paypalrestsdk 
 import stripe
 from datetime import datetime, timedelta
 from langdetect import detect  
